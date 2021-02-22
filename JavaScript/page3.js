d11331037150e16d17988b1a55be3a12cac28fe1
Jinkal Parmar <73112186+j100k-parmar@users.noreply.github.com>
2021-02-22 23:55:39 +0530

    login3

---
+++ JavaScript/page3.js
@@ -0,0 +1,11 @@
+function demo() {
+    // document.querySelector("#login").style.backgroundColor = "gray";
+    document.querySelector("#signup").style.top = "-180px";
+    document.querySelector("#signup").style.transition = "2s";
+
+}
+
+function demo1() {
+    document.querySelector("#signup").style.top = "300px";
+
+}
